9c425be7677c8a7c8740a2f288e919458e3aad44
xuchen <xuchen@tangusoft.com>
2017-08-23 17:20:32 +0800

    revert 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD:src/views/app/index.js
-=======
 import logo from './logo.svg';
 import './App.css';
->>>>>>> parent of f1a33fe... add css module:src/App.js
 
 class App extends Component {
   render() {
@@ -13,11 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-<<<<<<< HEAD:src/views/app/index.js
-        <p >
-=======
         <p className="App-intro">
->>>>>>> parent of f1a33fe... add css module:src/App.js
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
