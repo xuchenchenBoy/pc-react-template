2c2b035d0b1311ada14115c3f82a2f17e7c38941
xuchen <xuchen@tangusoft.com>
2017-08-23 15:37:42 +0800

    remove sass support

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import _styles from './index.css';
 
 class App extends Component {
   render() {
@@ -8,7 +7,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className={_styles.appHeader}>
+        <p >
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
