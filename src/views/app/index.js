81694debcda0f5a3d0337c04ea8a377ebda58ad5
xuchen <xuchen@tangusoft.com>
2017-08-24 13:59:00 +0800

    add airbnb eslint and close lint when run start

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import _styles from './index.css';
 
 class App extends Component {
+  onTestProxy() {
+    console.log(this);
+    fetch('/loginAuthor', { method: 'POST' });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
-        <p className={_styles.contentWrp}>
+        <p className={_styles.contentWrp} onClick={this.onTestProxy}>
           To get started hello
         </p>
-
       </div>
     );
   }
