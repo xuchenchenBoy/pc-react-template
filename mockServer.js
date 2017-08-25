5e7d1d791984c2c87a50a2b8007f5939db6c1c0e
xuchen <xuchen@tangusoft.com>
2017-08-25 17:46:21 +0800

    modify mock middleware

--- mockServer.js
+++ mockServer.js
@@ -12,6 +12,13 @@ server.use(function (req, res, next) {
     // Converts POST to GET and move payload to query params
     // This way it will make JSON Server that it's GET request
     req.method = 'GET'
+
+    // 匹配url,截取最后的一段param
+    if (req.url) {
+      splitUrl = req.url.split("/");
+      req.url = "/" + splitUrl[splitUrl.length - 1];
+    }
+
     req.query = req.body
   }
   // Continue to JSON Server router
--- src/views/app/index.js
+++ src/views/app/index.js
@@ -11,7 +11,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
