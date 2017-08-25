7928249ac8c6abda90b413844cecf01b3c7dd120
xuchen <xuchen@tangusoft.com>
2017-08-25 17:09:47 +0800

    add ignore file

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -11,6 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <div className="App-header">
           <h2>Welcome to React</h2>
         </div>
